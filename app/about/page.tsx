93891fd8f11097a8e7cb8ff3dbb470b26c33cf50
Jake Spievak <jakespvk@gmail.com>
2025-11-02 19:37:23 -0800

    Many updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,21 +11,81 @@ const about = () => {
     <>
       <Headline text={"About SunsetKimcare"} />
 
-      <div className="text-center lg:w-2/3 px-5 mx-auto my-auto">
-        <p className="text-2xl">Hello, welcome to my page
-          About me yada yada blah blah blah
-          I love skin and clear skin is my passion
-          fill the space fill the space fill the space
-          filling more space and more space and more space
-          tesitng the extra space and the width restrictions
+      <div className="lg:w-4/5 px-5 mx-auto my-auto">
+        <p className="text-xl py-3">
+          Hi! I’m Kimmy Ancheta, a licensed esthetician based in Long Beach, CA but also offering services in Santa Ana. I specialize in corrective facials, acne care, glow-focused treatments, and Brow Laminations!
+        </p>
+        <p className="text-xl py-3">
+          My approach to skincare blends a mix of advanced education, relaxation, and results-driven techniques so every client leaves feeling confident and cared for; both inside and out!
+        </p>
+        <p className="text-xl py-3">
+          My journey into esthetics started when I was in middle school. I struggled so much with my own skin over the years and became obsessed with learning how to heal it / I fell in love with the feeling of helping someone feel confident or even just good in their own skin. Since then, I’ve made it my mission to create a space where skin REALLY meets self-care; where each treatment feels like both a reset and a little celebration of you.
+        </p>
+        <p className="text-xl py-3">
+          When I’m not in the treatment room, you can find me teaching esthetics to future estheticians, brain-rotting on social media, yelling into a mic in karaoke rooms, watching shows & movies with loved ones, and sometimes you’ll even see me on a big screen; acting or modeling! I’m passionate about continuing skin education, staying inspired, and bringing my clients the latest in advanced treatments that truly work.
+        </p>
+        <p className="text-xl py-3">
+          Whether you’re here for a glow-up, a deep reset, or to start your skincare journey, I can’t wait to meet you and help you fall in love with your skin again. 🤍
         </p>
       </div>
 
       <div className="text-center lg:w-2/3 mx-auto my-auto">
 
         <div className={fraunces.className}>
+          <h1 id="mainTitle" className="text-7xl pt-10 bg-gradient-to-r from-amber-600 via-amber-300 to-amber-500 inline-block text-transparent bg-clip-text">
+            Signature Facials
+          </h1>
+          <h3 className="text-3xl pb-6">Select a facial:</h3>
+        </div>
+
+      </div>
+
+      <div className="lg:2/3 mx-auto my-auto min-w-fit">
+        <div className="flex flex-wrap justify-center">
+          <ServicesCards
+            cardTitle={"☀️ Day Dream"}
+            cardContent={"Personalized facial for luminous, refreshed skin."}
+            cardImgSrc={"https://images.squarespace-cdn.com/content/v1/54ab19d1e4b01ff9ee6258e2/1428977878229-B4ME0YAK6RU0P83JO3GL/iStock_000026513352_Large.jpg?format=1500w"}
+            cardLinkTo={"/booking/daydream"}
+            cardOverlayContent={"A 65-minute personalized facial designed to cleanse, exfoliate, and hydrate while melting away tension. Each step is customized to your skin’s unique needs, leaving it soft, balanced, and radiant, the perfect reset for your complexion and your mind."}
+            tags={["65 min"]}
+            price={"$55"}
+          />
+          <ServicesCards
+            cardTitle={"☀️ Sunset Reset"}
+            cardContent={"Quick refresh to restore skin harmony."}
+            cardImgSrc={"https://frasada.com/wp-content/uploads/2017/09/W-Euro.jpg"}
+            cardLinkTo={"/booking"}
+            cardOverlayContent={"A 45-minute express treatment made for returning clients or anyone needing a quick renewal. This facial helps maintain your skin’s clarity and balance between full sessions, featuring gentle exfoliation, hydration, and a soothing massage for that fresh, just-treated feel."}
+            tags={["45 min"]}
+            price={"$45"}
+          />
+          <ServicesCards
+            cardTitle={"☀️ Acne Afterglow"}
+            cardContent={"Clear, calm, and restore skin balance."}
+            cardImgSrc={"https://cdn.prod.website-files.com/6500a07711e91a1b23e39f7b/6509cf8a9cb78b9676a2dafe_AdobeStock_308584000-min.jpeg"}
+            cardLinkTo={"/booking"}
+            cardOverlayContent={"A 75-minute treatment designed for acne-prone or congested skin. This facial combines deep cleansing, gentle exfoliation, extractions, and light therapy to reduce inflammation and promote clarity. The result? Calm, refined, and balanced skin that feels healthy and renewed."}
+            tags={["75 min"]}
+            price={"$65"}
+          />
+          <ServicesCards
+            cardTitle={"☀️ Radiance Revival"}
+            cardContent={"Hydrate, replenish, and bring back vitality."}
+            cardImgSrc={"https://handandstone.com/_next/image/?url=https%3A%2F%2Fimages.ctfassets.net%2F8a0nz9cb9x1e%2F3tkJUfLc3d53YTLTtYRCSF%2F692bd5398df0a906bbdeb13bad14520a%2FService_Detail_Page-What-s_Included-Hydrating_Facial-Desktop.jpg&w=3840&q=75"}
+            cardLinkTo={"/booking"}
+            cardOverlayContent={"A 75-minute rejuvenating treatment that deeply nourishes and strengthens the skin barrier. Featuring a hydrating cleanse, antioxidant-infused products, and a custom Hydrojelly mask, this facial restores moisture, supports repair, and leaves your complexion visibly smoother and more vibrant."}
+            tags={["75 min"]}
+            price={"$65"}
+          />
+        </div>
+      </div>
+
+      <div className="text-center lg:w-2/3 mx-auto my-auto">
+
+        <div className={fraunces.className}>
           <h1 id="mainTitle" className="text-7xl p-10 bg-gradient-to-r from-amber-600 via-amber-300 to-amber-500 inline-block text-transparent bg-clip-text">
-            Services
+            Facial Packages
           </h1>
         </div>
 
@@ -34,60 +94,86 @@ const about = () => {
       <div className="lg:2/3 mx-auto my-auto min-w-fit">
         <div className="flex flex-wrap justify-center">
           <ServicesCards
-            cardTitle={"☀️ Facials"}
-            cardContent={"We offer many different facials, from vegan to pumpkin"}
+            cardTitle={"Glow Getter Package"}
+            cardContent={""}
             cardImgSrc={"https://media.allure.com/photos/5c2e8ec54325fe2d62c0943a/16:9/w_2560%2Cc_limit/how-often-should-you-get-a-facial-lede.jpg"}
-            cardLinkTo={"/booking"}
-            cardOverlayContent={"Completely customized facial"}
+            cardLinkTo={"/booking/daydream"}
+            cardOverlayContent={"Designed for anyone craving a reset, this package pairs two of my most-loved treatments to revive dull, depleted skin. Think of it as a double dose of hydration, renewal, and confidence; leaving you with a bright, rested complexion and that unmistakable “I’ve been taking good care of myself” energy."}
+            packageItems={["Day Dream Facial", "Radiance Revival Facial (includes jelly mask)", "1 FREE high frequency add on"]}
+            price={"$105 ($125 value)"}
           />
           <ServicesCards
-            cardTitle={"☀️ Facials"}
-            cardContent={"Keep that pesky mustache in check"}
+            cardTitle={"Self Care Series"}
+            cardContent={""}
             cardImgSrc={"https://www.shutterstock.com/image-photo/cosmetologist-performs-hydropiling-beauty-salon-600nw-2208041711.jpg"}
             cardLinkTo={"/booking"}
-            cardOverlayContent={"We offer many different facials, from vegan to pumpkin"}
+            cardOverlayContent={"A 45-minute express treatment made for returning clients or anyone needing a quick renewal. This facial helps maintain your skin’s clarity and balance between full sessions, featuring gentle exfoliation, hydration, and a soothing massage for that fresh, just-treated feel."}
+            packageItems={["2 Day Dream Facials", "1 FREE jelly mask add on"]}
+            price={"$110 ($120 value)"}
           />
           <ServicesCards
-            cardTitle={"Waxing! hehe"}
-            cardContent={"Keep that pesky mustache in check (and other things)"}
+            cardTitle={"Ultimate Radiance Retreat"}
+            cardContent={""}
             cardImgSrc={"https://t3.ftcdn.net/jpg/01/44/24/88/360_F_144248813_1PduH2CEnX2mdR9UpVNerzZs6Kv64qsw.jpg"}
             cardLinkTo={"/booking"}
             cardOverlayContent={"We offer many different facials, from vegan to pumpkin"}
+            packageItems={["Radiance Revival Facial (includes jelly mask)", "Acne Afterglow Facial (includes LED)", "1 FREE High Frequency add-on", "1 Dermaplaning add-on included"]}
+            price={"$140 ($155 value)"}
           />
           <ServicesCards
-            cardTitle={"Facials!"}
-            cardContent={"We offer many different facials, from vegan to pumpkin"}
+            cardTitle={"Clear Horizon Package"}
+            cardContent={""}
             cardImgSrc={"https://media.allure.com/photos/5c2e8ec54325fe2d62c0943a/16:9/w_2560%2Cc_limit/how-often-should-you-get-a-facial-lede.jpg"}
             cardLinkTo={"/booking"}
             cardOverlayContent={"We offer many different facials, from vegan to pumpkin"}
+            packageItems={["3 Acne Afterglow Facials (includes blue LED)", "FREE High Frequency on all sessions"]}
+            price={"$180 ($210 value)"}
+          />
+        </div>
+      </div>
+
+      <div className="text-center lg:w-2/3 mx-auto my-auto">
+
+        <div className={fraunces.className}>
+          <h1 id="mainTitle" className="text-7xl p-10 bg-gradient-to-r from-amber-600 via-amber-300 to-amber-500 inline-block text-transparent bg-clip-text">
+            Facial Add-Ons
+          </h1>
+        </div>
+
+      </div>
+      <div className="lg:2/3 mx-auto my-auto min-w-fit">
+        <div className="flex flex-wrap justify-center">
+          <ServicesCards
+            cardTitle={"☀️ High Frequency"}
+            cardContent={""}
+            cardImgSrc={"https://static.wixstatic.com/media/2fdacb_ee6a2b770650475293e4fd855667ad55~mv2.jpg/v1/fill/w_640,h_484,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/2fdacb_ee6a2b770650475293e4fd855667ad55~mv2.jpg"}
+            cardLinkTo={"/booking/daydream"}
+            cardOverlayContent={"Boost circulation and target breakouts with gentle electrical currents that calm, purify, and support skin healing."}
+            notBookable={true}
           />
           <ServicesCards
-            cardTitle={"Dermabrasion!"}
-            cardContent={"Keep that pesky mustache in check"}
-            cardImgSrc={"https://www.shutterstock.com/image-photo/cosmetologist-performs-hydropiling-beauty-salon-600nw-2208041711.jpg"}
+            cardTitle={"☀️ Custom Hydrojelly Mask"}
+            cardContent={""}
+            cardImgSrc={"https://www.honeybesthetics.com/cdn/shop/files/IMG-1851.png?v=1746437836"}
             cardLinkTo={"/booking"}
-            cardOverlayContent={"We offer many different facials, from vegan to pumpkin"}
+            cardOverlayContent={"A cooling, skin-loving mask tailored to your needs; adds hydration, soothes irritation, and seals in nutrients for a supple finish."}
+            notBookable={true}
           />
           <ServicesCards
-            cardTitle={"Waxing! hehe"}
-            cardContent={"Keep that pesky mustache in check (and other things)"}
-            cardImgSrc={"https://t3.ftcdn.net/jpg/01/44/24/88/360_F_144248813_1PduH2CEnX2mdR9UpVNerzZs6Kv64qsw.jpg"}
+            cardTitle={"☀️ LED Light Therapy"}
+            cardContent={""}
+            cardImgSrc={"https://images.squarespace-cdn.com/content/v1/68061840f8f9a419beb845be/13eaaaad-3b37-4e8d-b121-c25678549109/Maryville-Acupuncture_LED-Light-Therapy_Header-Image.png"}
             cardLinkTo={"/booking"}
-            cardOverlayContent={"We offer many different facials, from vegan to pumpkin"}
+            cardOverlayContent={"Harness the power of light energy to refine texture, calm redness, and promote a more even, healthy complexion."}
+            notBookable={true}
           />
           <ServicesCards
-            cardTitle={"Facials!"}
-            cardContent={"We offer many different facials, from vegan to pumpkin"}
-            cardImgSrc={"https://media.allure.com/photos/5c2e8ec54325fe2d62c0943a/16:9/w_2560%2Cc_limit/how-often-should-you-get-a-facial-lede.jpg"}
+            cardTitle={"☀️ Dermaplaning"}
+            cardContent={""}
+            cardImgSrc={"https://images.squarespace-cdn.com/content/v1/5c4f6ba1e2ccd1ee6075495d/b46da5d5-865c-4244-8bab-6d8abd83f222/dermaplaning-benefits.jpg"}
             cardLinkTo={"/booking"}
-            cardOverlayContent={"We offer many different facials, from vegan to pumpkin"}
-          />
-          <ServicesCards
-            cardTitle={"Dermabrasion!"}
-            cardContent={"Keep that pesky mustache in check"}
-            cardImgSrc={"https://www.shutterstock.com/image-photo/cosmetologist-performs-hydropiling-beauty-salon-600nw-2208041711.jpg"}
-            cardLinkTo={"/booking"}
-            cardOverlayContent={"We offer many different facials, from vegan to pumpkin"}
+            cardOverlayContent={"Instantly smooth and brighten with this gentle exfoliating treatment that removes dull surface buildup and peach fuzz for a flawless finish."}
+            notBookable={true}
           />
         </div>
       </div>
--- app/about/services_cards.tsx
+++ app/about/services_cards.tsx
@@ -2,7 +2,10 @@
 
 import React from 'react'
 
-const ServicesCards = ({ cardTitle, cardContent, cardImgSrc, cardLinkTo, cardOverlayContent }: { cardTitle: string, cardContent: string, cardImgSrc: string, cardLinkTo: string, cardOverlayContent: string }) => {
+import { Coiny } from 'next/font/google'
+const fraunces = Coiny({ weight: '400', subsets: ['latin'] })
+
+const ServicesCards = ({ cardTitle, cardContent, cardImgSrc, cardLinkTo, cardOverlayContent, packageItems, tags, notBookable, price }: { cardTitle: string, cardContent: string, cardImgSrc: string, cardLinkTo: string, cardOverlayContent?: string, packageItems?: string[], tags?: string[], notBookable?: boolean, price?: string }) => {
   return (
     <>
       <div className="relative group">
@@ -14,27 +17,172 @@ const ServicesCards = ({ cardTitle, cardContent, cardImgSrc, cardLinkTo, cardOve
                 src={cardImgSrc}
                 alt="Facial" />
             </figure>
-            <div className="card-body h-56">
-              <h2 id="card_title" className="card-title text-2xl">{cardTitle}</h2>
-              <p>{cardContent}</p>
+            <div className={(packageItems !== undefined && packageItems.length > 0) ? "card-body h-64" : "card-body h-44"}>
+              <div className={fraunces.className}>
+                <h2 id="card_title" className="card-title text-3xl tracking-wide pb-3">{cardTitle}</h2>
+              </div>
+              {packageItems !== undefined && packageItems.length > 0
+                ?
+                <ul className="mx-2 list-disc list-inside my-auto">
+                  {packageItems.map((item) => <li className="">{item}</li>)}
+                </ul>
+                :
+                null}
+              {(cardContent !== undefined && cardContent.length > 0) ? <p className="text-lg">{cardContent}</p> : null}
+              {(tags) || (price) ?
+                <div className="card-actions justify-end">
+                  {(tags) ? tags.map((tag: string) =>
+                    <div className="text-warning">{tag}</div>
+                  ) : null}
+                  <div>{price}</div>
+                </div>
+                : null}
             </div>
           </div>
         </div>
-        <div className="p-5 absolute inset-0 opacity-0 hover:opacity-100 transition duration-500 ease-in-out bg-blend-overlay">
+        <div className="p-5 absolute inset-0 opacity-0 hover:opacity-100 bg-blend-overlay">
           <div
-            className="card w-96 h-112 bg-base-300/50 bg-blend-overlay shadow-[0_35px_60px_-15px_rgb(0,0,0,0.35)]"
-            style={{
-              backgroundImage: "url(https://t4.ftcdn.net/jpg/05/01/83/79/360_F_501837926_xvM4Ym7pql243YOrjmct5NCXjFTxz11v.jpg)",
-            }}>
-            <p className="p-5">{cardOverlayContent}</p>
-            <div className="card-actions justify-center items-end grow">
-              <a role="button" className="btn btn-accent mb-10 text-lg px-4 py-2" href={cardLinkTo}>Book Now</a>
-            </div>
+            className="card w-96 h-full backdrop-blur-xl bg-base-300/70 bg-blend-overlay shadow-[0_7px_20px_7px_rgb(230,150,23,0.7)] transition duration-200 ease-in-out border-1 border-base-200"
+          >
+            {cardOverlayContent !== undefined && cardOverlayContent.length > 0
+              ?
+              <p className="p-5 pb-0 text-center text-lg {notBookable ? my-auto : my-0}">{cardOverlayContent}</p>
+              :
+              null
+            }
+            {price ? <strong className="text-center my-auto">{price}</strong> : null}
+            {notBookable ? null :
+              <div className="card-actions justify-center items-end grow">
+                <FancyButton cardLinkTo={cardLinkTo} />
+              </div>
+            }
           </div>
-        </div>
-      </div>
+        </div >
+      </div >
     </>
   )
 }
 
 export default ServicesCards
+
+const FancyButton = ({ cardLinkTo }: { cardLinkTo: string }) => {
+  return (
+    <a role="button" className="btn btn-accent mb-10 text-lg px-4 py-2 jellybtn" href={cardLinkTo}>
+      Book Now
+      <div className="star-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          // xml:space="preserve"
+          version="1.1"
+          // style={shapeRendering:"geometricPrecision", textRendering:"geometricPrecision", imageRendering:"optimizeQuality", fillRule:"evenodd", clipRule:"evenodd"}
+          viewBox="0 0 784.11 815.53"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div className="star-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          // xml:space="preserve"
+          version="1.1"
+          // style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+          viewBox="0 0 784.11 815.53"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div className="star-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          // xml:space="preserve"
+          version="1.1"
+          // style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+          viewBox="0 0 784.11 815.53"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div className="star-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          // xml:space="preserve"
+          version="1.1"
+          // style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+          viewBox="0 0 784.11 815.53"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div className="star-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          // xml:space="preserve"
+          version="1.1"
+          // style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+          viewBox="0 0 784.11 815.53"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div className="star-6">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          // xml:space="preserve"
+          version="1.1"
+          // style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+          viewBox="0 0 784.11 815.53"
+        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            ></path>
+          </g>
+        </svg>
+      </div>
+    </a>
+  )
+}
