53356a823cc32fe8f1fbc43453e047e55b46d7eb
Jake Spievak <jakespvk@gmail.com>
2025-12-06 12:34:39 -0800

    updates

--- components/ui/booking-request-warning-text.tsx
+++ components/ui/booking-request-warning-text.tsx
@@ -1,6 +1,6 @@
 export default function BookingRequestWarningText() {
   return (
-    <div className="md:flex flex-col justify-center items-center text-lg font-bold mx-5 text-red-700">
+    <div className="md:flex flex-col justify-center items-center text-lg font-bold mx-5 text-red-600">
       <p>Selecting a date and time sends a request, not a confirmed appointment.</p>
       <p className="pt-1 pb-5">You’ll receive a message or email once your booking has been approved.</p>
     </div>
