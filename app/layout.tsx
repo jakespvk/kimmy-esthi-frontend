f7d63e457a330b2d5112b10d630fb389405c6e6c
Jake Spievak <jakespvk@gmail.com>
2025-12-05 19:12:23 -0800

    fix bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
 import './globals.css'
-import type { Metadata, Viewport } from 'next'
+import type { Metadata } from 'next'
 import { montserrat } from './fonts'
 
 export const metadata: Metadata = {
   title: 'Sunset Kimcare',
   description: 'Sunset Kimcare Esthetician Services',
 }
-
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-  viewportFit: 'cover',
-  themeColor: '#bb4d00',
-}
+//
+// export const viewport: Viewport = {
+//   width: 'device-width',
+//   initialScale: 1,
+//   viewportFit: 'cover',
+//   themeColor: '#bb4d00',
+// }
 
 export default function RootLayout({
   children,
