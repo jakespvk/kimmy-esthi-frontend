20cc8d7d8deaf609f6b929908538dbaf78a748b4
Jake Spievak <jakespvk@gmail.com>
2025-12-18 20:31:12 -0800

    more updates to consent form

---
+++ app/booking/scheduleAppointment/consentForm/initialed-statements.ts
@@ -0,0 +1,4 @@
+export const initialedStatements: string[] = [
+  "I agree that blah blah blah",
+  "I also agree that blah blah blah"
+]
--- app/booking/scheduleAppointment/consentForm/page.tsx
+++ app/booking/scheduleAppointment/consentForm/page.tsx
@@ -1,9 +1,17 @@
 import SignaturePad from '@/components/ui/signature-pad';
 import { Eraser, Save } from 'lucide-react';
+import { initialedStatements } from './initialed-statements';
+import Headline from '@/app/about/headline';
 
 const ConsentForm: React.FC = () => {
   return (
-    <div>
+    <div className='mx-5 lg:flex justify-center'>
+      <Headline text="Consent Form" />
+      <ul>
+        {initialedStatements.map((stmt, idx) =>
+          <li key={idx}>{stmt}</li>
+        )}
+      </ul>
       <SignaturePad
         className="max-w-64"
         penColor="hsl(var(--foreground))"
