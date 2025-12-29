329876332ef1a8313e1f4c58ecf1b8952a5d2738
Jake Spievak <jakespvk@gmail.com>
2025-12-28 20:19:28 -0800

    services editor complete

--- app/admin/dashboard/admin-service-details.tsx
+++ app/admin/dashboard/admin-service-details.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import { Service } from "@/app/types";
-import { ServiceEditor } from "./service-editor";
+import ServiceEditor from "./service-editor";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function AdminServiceDetails({ service }: { service: Service }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   function showModal() {
     {/* @ts-ignore */ }
     document.getElementById("service-detail-modal")?.showModal();
   }
 
-  function editService() {
-    console.log("edit service")
-    return <ServiceEditor service={service} />
+  function toggleIsEditing() {
+    setIsEditing(!isEditing);
   }
 
   return (
@@ -22,11 +27,11 @@ export default function AdminServiceDetails({ service }: { service: Service }) {
           {Object.entries(service).map((field, index) => (
             <div className="my-5" key={index}>
               <label className="font-bold">{field[0]}:&nbsp;</label>
-              <p>{field[1]}</p>
+              {isEditing ? <Input defaultValue={field[1]} /> : <p>{field[1]}</p>}
             </div>
           ))}
           <div className="modal-action">
-            <button className="btn" onClick={() => editService()}>Edit</button>
+            <button className="btn" onClick={toggleIsEditing}>{isEditing ? "Save Changes" : "Edit"}</button>
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
               <button className="btn">Close</button>
