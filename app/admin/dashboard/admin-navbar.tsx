9834c225c172ea0b535314de43374a763533fab9
Jake Spievak <jakespvk@gmail.com>
2026-01-28 08:26:51 -0800

    backdrop blur behind admin navbar

--- app/admin/dashboard/admin-navbar.tsx
+++ app/admin/dashboard/admin-navbar.tsx
@@ -9,7 +9,7 @@ export default function AdminNavbar() {
   }
 
   return (
-    <div className="sticky top-0 z-10">
+    <div className="sticky top-0 z-10 backdrop-blur-xs">
       <nav className="relative flex flex-wrap justify-center p-2 gap-2 *:bg-base-200 *:hover:bg-base-300 *:scroll-mt-4">
         <Button variant="outline" onClick={() => scrollToElement("create-appointments")}>Create Appointments</Button>
         <Button variant="outline" onClick={() => scrollToElement("appointments-list")}>Appointments List</Button>
